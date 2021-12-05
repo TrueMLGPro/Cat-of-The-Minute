a0970c97aa8a3f5c82c8a9976a137482c912329b
TrueMLGPro <revit005@gmail.com>
2021-12-05 23:59:55 +0200

    Updates

---
+++ precache-manifest.ebf9943802634871b258f30dfca24f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b62eacff71f5a782bf36412bc5bf12",
+    "url": "/Cat-of-The-Minute/index.html"
+  },
+  {
+    "revision": "cbf0019e29cf957421fc",
+    "url": "/Cat-of-The-Minute/static/css/main.62562e34.chunk.css"
+  },
+  {
+    "revision": "1df5936af09d5b8448a4",
+    "url": "/Cat-of-The-Minute/static/js/2.f5704b5d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Cat-of-The-Minute/static/js/2.f5704b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5856c0e3925cfe84b17",
+    "url": "/Cat-of-The-Minute/static/js/3.d703e484.chunk.js"
+  },
+  {
+    "revision": "cbf0019e29cf957421fc",
+    "url": "/Cat-of-The-Minute/static/js/main.fa70df48.chunk.js"
+  },
+  {
+    "revision": "e5cd63da5655a230b927",
+    "url": "/Cat-of-The-Minute/static/js/runtime-main.dade8757.js"
+  }
+]);
